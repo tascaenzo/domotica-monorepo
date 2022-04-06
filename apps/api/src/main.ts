6f12497c8bac2879e22d570beb56da03dbb9598b
enzo <enzo@iMac-Pro-enzo.homenet.telecomitalia.it>
2022-04-07 01:06:35 +0200

    feat: 🎸 minority fix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,44 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+// import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  const config = app.get(ConfigService);
+  const port = config.get('API_PORT');
+  // const allowedCors = config.get('CORS_ORIGINS').split(',');
+
+  app.enableCors({
+    //origin: ['http://localhost:8080', 'http://192.168.1.70:8080'],
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: false,
+  });
+
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
+
+  // Security middleware
+  app.use(helmet());
+  // app.use(cookieParser());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: config.get('ENV') === 'development' ? Infinity : 100,
+    })
+  );
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(port, '0.0.0.0');
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `� Application is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
